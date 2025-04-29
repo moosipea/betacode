4c06c3197bd5330e1bffac8f2c92d760a9fa8228
moosipea <mattiasmcvolt@gmail.com>
2025-04-30 01:20:12 +0300

    i think everything is hooked up

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use core::str;
-
 use wasm_minimal_protocol::wasm_func;
+use unicode_normalization::UnicodeNormalization;
 
 #[cfg(target_arch = "wasm32")]
 wasm_minimal_protocol::initiate_protocol!();
@@ -10,7 +9,8 @@ use parser::parse_betacode;
 
 #[cfg_attr(target_arch = "wasm32", wasm_func)]
 pub fn bcode(input: &[u8]) -> Result<Vec<u8>, String>{
-    let string = str::from_utf8(input).map_err(|err| err.to_string())?;
+    let string = std::str::from_utf8(input).map_err(|err| err.to_string())?;
     let betacode = parse_betacode(string).map_err(|err| err.to_string())?;
-    todo!()
+    let normalized: String = betacode.nfc().collect();
+    Ok(normalized.into_bytes())
 }
